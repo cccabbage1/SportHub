65fe4a4574063b983b0e4887fd166d91273650d8
Lucas Chernoff <mich8112@colorado.edu>
2024-06-28 12:57:54 -0600

    trying to add to main

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -1,14 +1,136 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars');
+const path = require('path');
+const pgp = require('pg-promise')();
+const bodyParser = require('body-parser');
+const session = require('express-session');
+require('dotenv').config();
 
 const hostname = '0.0.0.0';
 const port = 1234;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+// -------------------------------------  APP CONFIG   ----------------------------------------------
+
+// Create `ExpressHandlebars` instance and configure the layouts and partials dir.
+const hbs = handlebars.create({
+  extname: 'hbs',
+  layoutsDir: path.join(__dirname, 'views', 'layouts'),
+  partialsDir: path.join(__dirname, 'views', 'partials'),
 });
 
-server.listen(port, hostname, () => {
+// Register `hbs` as our view engine using its bound `engine()` function.
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set Session
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET, // Replace with your own secret
+    saveUninitialized: true,
+    resave: true,
+  })
+);
+
+
+// -------------------------------------  DB CONFIG AND CONNECT   ---------------------------------------
+const dbConfig = {
+  host: 'db',
+  port: 5432,
+  database: process.env.POSTGRES_DB, // Replace with your database name
+  user: process.env.POSTGRES_USER, // Replace with your database user
+  password: process.env.POSTGRES_PASSWORD, // Replace with your database password
+};
+const db = pgp(dbConfig);
+
+// DB Test
+db.connect()
+  .then(obj => {
+    console.log('Database connection successful');
+    obj.done(); // success, release the connection;
+  })
+  .catch(error => {
+    console.log('ERROR', error.message || error);
+  });
+
+// -------------------------------------  ROUTES for login.hbs   ----------------------------------------------
+const user = {
+  user_id: undefined,
+  username: undefined,
+  password: undefined,
+  first_name: undefined,
+  last_name: undefined,
+  email: undefined
+};
+
+
+
+app.get('/login', (req, res) => {
+  res.render('pages/login');
+});
+
+// Login submission
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const query = 'SELECT * FROM userinfo WHERE username = $1 and password = $2 LIMIT 1';
+  const values = [username, password];
+
+  // Get the student_id based on the emailid
+  db.one(query, values)
+    .then(data => {
+      user.user_id = data.user_id;
+      user.username = username;
+      user.password = password;
+      user.first_name = data.first_name;
+      user.last_name = data.last_name;
+      user.email = data.email;
+
+      req.session.user = user;
+      req.session.save();
+
+      res.redirect('/');
+    })
+    .catch(err => {
+      console.log(err);
+      res.redirect('/login');
+    });
+});
+
+// Authentication middleware.
+const auth = (req, res, next) => {
+  if (!req.session.user) {
+    return res.redirect('/login');
+  }
+  next();
+};
+
+app.use(auth);
+
+// -------------------------------------  ROUTES for home.hbs   ----------------------------------------------
+
+app.get('/', (req, res) => {
+  res.render('pages/home', {
+    username: req.session.user.username,
+    password: req.session.user.password,
+    first_name: req.session.user.first_name,
+    last_name: req.session.user.last_name,
+    email: req.session.user.email,
+  });
+});
+
+// -------------------------------------  ROUTES for logout.hbs   ----------------------------------------------
+
+app.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.render('pages/logout');
+});
+
+// -------------------------------------  START THE SERVER   ----------------------------------------------
+
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
